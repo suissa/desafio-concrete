d9221d2ca79fbc4893ff59c8826790ee4bb878a1
Suissa <jnascimento@gmail.com>
2016-04-08 23:31:33 -0300

    Inicio de isPhoneNumber

--- modules/User/quarks/isPhoneNumber.js
+++ modules/User/quarks/isPhoneNumber.js
@@ -1,11 +1,12 @@
 'use strict';
 
 module.exports = (value) => {
+  const regex = /^$/i;
   const isEmpty = require('./isEmpty')(value);
   const isString = require('./isString')(value);
 
   if(isEmpty) return false;
   if(!isString) return false;
 
-  return (value.length === 2);
+  return regex.test(value);
 }
